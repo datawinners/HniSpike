d45bc9272bbb248660e7a8565769b569624455f4
Shweta <shwetags@thoughtworks.com>
2011-03-08 15:22:24 +0530

    Modified settings.py

--- formservicespike/settings.py
+++ formservicespike/settings.py
@@ -8,12 +8,18 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+DATABASE_NAME = 'datadict',
+SERVER_ADDRESS = 'http://localhost',
+SERVER_PORT = '5984',
 
 DATABASES = {
     'default': {
-        DATABASE_NAME : 'datadict',
-        SERVER_ADDRESS : 'http://localhost',
-        SERVER_PORT : '5984'
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'QnA.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -79,6 +85,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -86,6 +93,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django-nose',
+    'formservice',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
